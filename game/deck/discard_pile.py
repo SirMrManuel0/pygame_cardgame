c18df0c88168062ed3d8a47fe8151be22d040064
SirMrManuel0 <139504269+SirMrManuel0@users.noreply.github.com>
2025-03-08 15:52:13 +0100

    add argument validation to discard_pile.py

--- game/deck/discard_pile.py
+++ game/deck/discard_pile.py
@@ -12,13 +12,17 @@ class DiscardPile(gd.Deck):
         return self._cards[0]
 
     def add(self, card: gd.Card) -> None:
+        assertion.assert_type(card, gd.Card, ArgumentError, code=ArgumentCodes.NOT_CARD)
         self._cards.insert(0, card)
 
     def get_all(self) -> list:
         return self._cards
 
     def set(self, cards: list) -> None:
+        assertion.assert_type(cards, list, ArgumentError, code=ArgumentCodes.NOT_LIST)
         self._cards = cards
 
     def reset(self, game_deck, shuffle: int = gd.Shuffle.DUMP) -> None:
+        assertion.assert_type(game_deck, gd.GameDeck, ArgumentError, code=ArgumentCodes.NOT_GAME_DECK)
+        assertion.assert_type(shuffle, gd.Shuffle, ArgumentError, code=ArgumentCodes.NOT_SHUFFLE)
         game_deck.update(self, shuffle)
